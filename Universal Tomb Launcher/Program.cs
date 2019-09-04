9541dc1629999933c13539f417d6e256fc4b55a5
Nickelony <nickelkwn@gmail.com>
2019-09-04 22:41:43 +0200

    Major update with a customizable splash screen and a log cleaner.

--- Universal Tomb Launcher/Program.cs
+++ Universal Tomb Launcher/Program.cs
@@ -1,6 +1,7 @@
 using IWshRuntimeLibrary;
 using Microsoft.Win32;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -14,38 +15,64 @@ namespace UniversalTombLauncher
 		[STAThread]
 		private static void Main()
 		{
-			string exeDirectoryPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-
-			// Look for a valid game .exe file
-			foreach (string file in Directory.GetFiles(exeDirectoryPath, "*.exe", SearchOption.TopDirectoryOnly))
+			try
 			{
-				string fileName = Path.GetFileName(file).ToLower();
+				string exeDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+				string engineSubdirectory = Path.Combine(exeDirectory, "Engine");
 
-				if (fileName == "tomb2.exe" || fileName == "tomb3.exe" || fileName == "tomb4.exe" || fileName == "pctomb5.exe")
+				string validExeFilePath = string.Empty;
+
+				// Look for a valid game .exe file
+
+				// Try to find it in the /Engine/ directory first (if it exists)
+				if (Directory.Exists(engineSubdirectory))
 				{
-					// Install border fix for tomb4 games if it wasn't installed yet
-					if (fileName == "tomb4.exe" && IsRequiredWindowsVersion() && !IsBorderFixInstalled())
-						InstallBorderFix();
-
-					// Add the "Press CTRL key for SETUP" prompt for vanilla TR games
-					if (!System.IO.File.Exists(Path.Combine(exeDirectoryPath, "Tomb_NextGeneration.dll")))
+					foreach (string file in Directory.GetFiles(engineSubdirectory, "*.exe", SearchOption.TopDirectoryOnly))
 					{
-						using (FormSetupPrompt form = new FormSetupPrompt())
+						if (IsValidGameExeFile(file))
 						{
-							if (form.ShowDialog() == DialogResult.OK)
-								RunGame(file, true);
-							else
-								RunGame(file);
+							validExeFilePath = file;
+							break;
 						}
 					}
-					else // TRNG already has that feature
-						RunGame(file);
+				}
 
-					return;
+				// If the engine subdirectory doesn't exist or no valid .exe file was found in that directory
+				if (string.IsNullOrEmpty(validExeFilePath))
+				{
+					// Try to look for the file in the executing directory
+
+					foreach (string file in Directory.GetFiles(exeDirectory, "*.exe", SearchOption.TopDirectoryOnly))
+					{
+						if (IsValidGameExeFile(file))
+						{
+							validExeFilePath = file;
+							break;
+						}
+					}
+				}
+
+				if (string.IsNullOrEmpty(validExeFilePath)) // No valid .exe file was found
+					throw new ArgumentException("Couldn't find a valid game .exe file.");
+				else
+				{
+					// Install border fix for tomb4 games if it wasn't installed yet
+					if (Path.GetFileName(validExeFilePath).ToLower() == "tomb4.exe" && IsRequiredWindowsVersion() && !IsBorderFixInstalled())
+						InstallBorderFix();
+
+					using (FormSetupSplash form = new FormSetupSplash(Path.GetDirectoryName(validExeFilePath)))
+					{
+						if (form.ShowDialog() == DialogResult.OK) // Pressed CTRL
+							RunGame(validExeFilePath, true);
+						else // 1 second passed
+							RunGame(validExeFilePath);
+					}
 				}
 			}
-
-			MessageBox.Show("Couldn't find a valid game .exe file.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
 		}
 
 		private static void RunGame(string exeFilePath, bool setup = false)
@@ -77,7 +104,66 @@ namespace UniversalTombLauncher
 			{
 				if (System.IO.File.Exists(shortcutPath))
 					System.IO.File.Delete(shortcutPath);
+
+				MoveLogFiles(exeFilePath);
 			}
+		}
+
+		private static void MoveLogFiles(string exeFilePath)
+		{
+			string validExeDirectory = Path.GetDirectoryName(exeFilePath);
+			string logsDirectory = Path.Combine(validExeDirectory, "logs");
+
+			if (!Directory.Exists(logsDirectory))
+				Directory.CreateDirectory(logsDirectory);
+
+			string[] files = Directory.GetFiles(validExeDirectory);
+
+			foreach (string file in files)
+			{
+				string fileName = Path.GetFileName(file).ToLower();
+
+				if (fileName == "db_patches_crash.bin" || fileName == "detected crash.txt"
+					|| fileName == "lastextraction.lst" || fileName.EndsWith("_warm_up_log.txt"))
+				{
+					System.IO.File.Copy(file, Path.Combine(logsDirectory, Path.GetFileName(file)), true);
+					System.IO.File.Delete(file);
+				}
+				else if (fileName.StartsWith("last_crash_") && fileName.EndsWith(".txt"))
+				{
+					string[] existingLogFiles = Directory.GetFiles(logsDirectory, "last_crash_*.txt", SearchOption.TopDirectoryOnly);
+
+					string logTxtFilePath = string.Empty;
+
+					if (existingLogFiles.Length > 0)
+					{
+						List<int> existingNumbers = new List<int>();
+
+						foreach (string existingLogFile in existingLogFiles)
+							existingNumbers.Add(int.Parse(Path.GetFileNameWithoutExtension(existingLogFile).Split('_')[2]));
+
+						int nextLogNumber = existingNumbers.Max() + 1;
+
+						logTxtFilePath = Path.Combine(logsDirectory, "Last_Crash_" + nextLogNumber + ".txt");
+					}
+					else
+						logTxtFilePath = Path.Combine(logsDirectory, Path.GetFileName(file));
+
+					System.IO.File.Copy(file, logTxtFilePath, true);
+					System.IO.File.Delete(file);
+
+					string memFilePath = Path.Combine(Path.GetDirectoryName(file), Path.GetFileNameWithoutExtension(file) + ".mem");
+
+					if (System.IO.File.Exists(memFilePath))
+					{
+						System.IO.File.Copy(memFilePath, Path.Combine(logsDirectory, Path.GetFileNameWithoutExtension(logTxtFilePath) + ".mem"), true);
+						System.IO.File.Delete(memFilePath);
+					}
+				}
+			}
+
+			if (Directory.GetFileSystemEntries(logsDirectory).Length == 0)
+				Directory.Delete(logsDirectory);
 		}
 
 		private static void InstallBorderFix()
@@ -103,6 +189,12 @@ namespace UniversalTombLauncher
 				if (System.IO.File.Exists(sdbFilePath))
 					System.IO.File.Delete(sdbFilePath);
 			}
+		}
+
+		private static bool IsValidGameExeFile(string file)
+		{
+			string fileName = Path.GetFileName(file).ToLower();
+			return fileName == "tomb2.exe" || fileName == "tomb3.exe" || fileName == "tomb4.exe" || fileName == "pctomb5.exe";
 		}
 
 		private static bool IsBorderFixInstalled()
