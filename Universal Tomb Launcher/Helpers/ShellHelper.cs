20c314eabd9af612db85442dca5a0db1b958e568
Kewin Kupilas <k.kupilas@outlook.com>
2025-08-24 16:34:01 +0100

    Adjustment

--- Universal Tomb Launcher/Helpers/ShellHelper.cs
+++ Universal Tomb Launcher/Helpers/ShellHelper.cs
@@ -15,7 +15,7 @@ namespace UniversalTombLauncher.Helpers
 		/// <param name="exeFilePath">The file path to the executable for which the shortcut is being created.</param>
 		/// <param name="iconLocation">The file path to the icon to be used for the shortcut.</param>
 		/// <param name="args">The command-line arguments to be passed to the executable when the shortcut is used.</param>
-		/// <returns>The created shortcut as an <see cref="IPersistFile"/> instance.</returns>
+		/// <returns>The created shortcut as an <see cref="IPersistFile" /> instance.</returns>
 		public static IPersistFile CreateShortcutWithIcon(string exeFilePath, string iconLocation, string args)
 		{
 			var link = (IShellLink)new ShellLink();
@@ -31,7 +31,7 @@ namespace UniversalTombLauncher.Helpers
 		/// <summary>
 		/// Saves the provided shortcut to a temporary location and returns the path to the saved file.
 		/// </summary>
-		/// <param name="shortcut">The shortcut to be saved, represented as an <see cref="IPersistFile"/> instance.</param>
+		/// <param name="shortcut">The shortcut to be saved, represented as an <see cref="IPersistFile" /> instance.</param>
 		/// <param name="exeFilePath">The file path to the executable for which the shortcut is being created. This is used to derive the name of the shortcut file.</param>
 		/// <returns>The file path to the saved shortcut.</returns>
 		public static string SaveShortcut(IPersistFile shortcut, string exeFilePath)
